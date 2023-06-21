fb41feb6777f542d191b2a1b7e2d48a045b71a31
Sahil1525 <sahilsaha15255@gmail.com>
2023-06-21 14:05:20 +0530

    Backend

---
+++ DAY 14/Backend/Routes/UserRoute.js
@@ -0,0 +1,31 @@
+const express  = require("express");
+const userRoute = express.Router();
+const { UserModel } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+userRoute.get("/", (req,res) => {
+    res.send("User Registration");
+});
+
+userRoute.post("/register", async (req,res) => {
+    const { name, email, gender, pass, mobile } = req.body;
+    try{
+        const findUser = await UserModel.find({ email });
+        if (findUser.length === 0) {
+
+            bcrypt.hash(pass,3,async (err, hashed_pass) => {
+                if (err) {
+                    res.send({ msg: "error occured" });
+                } else {
+                    let user = new UserModel({ name, email, gender, pass: hashed_pass });
+                    await user.save();
+                    res.send({ msg: "User Registered Successfully" });
+                }
+            });
+        } else {
+            res.send({ msg: "User is already registered" });
+        } catch (err) {
+            res.send({ msg: err });
+        }
+});
